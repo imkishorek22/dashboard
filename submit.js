40b28be1367a7ada24887e592dbc5399057e4faa
imkishorek22 <91674171+imkishorek22@users.noreply.github.com>
2025-07-19 00:00:26 +0530

    Delete submit.js

--- submit.js
+++
@@ -1,8 +0,0 @@
-function validate(){
-var username=document.getElementById("UserName").value;
-var password=document.getElementById("Pass").value;
-if( username =="admin" && password == "admin@123" ){
-    alert("login Successfull");
-    Window.location="dashboard.html";
-}
-}
